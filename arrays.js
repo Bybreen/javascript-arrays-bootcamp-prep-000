06521c12530b7f38829842f4a8245c56da19d238
Bybreen Samuels <bybreensamuels@googlemail.com>
2019-04-14 14:29:52 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,3 @@
-var ingredient1 = "bread"
-var ingredient2 = "mild cheese"
-var ingredient3 = "sharp cheese"
-var ingredient4 = "butter"
-var ingredient5 = "tomato"
-var ingredient6 = "garlic"
-
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
 var exercise = ["addElementToBeginningOfArray","running", "football", "hockey", "swimmming"];
 
 var coding = ["destructivelyAddElementToBeginningOfArray", "websites", "mobile apps", "frontend", "backend"];
